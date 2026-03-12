f059a7229f94a82777a29abefd2d6978f6432b74
Tung Tom <tungton2005@gmail.com>
2026-03-12 11:11:09 +0700

    Add project files.

---
+++ TonThatTung_2123110363/Controllers/CategorisController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TonThatTung_2123110363.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategorisController : ControllerBase
+    {
+        // GET: api/<CategorysController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CategorysController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CategorysController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CategorysController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CategorysController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
